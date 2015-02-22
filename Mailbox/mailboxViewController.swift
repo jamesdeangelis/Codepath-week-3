ebc599c48d4ac6bc4bad3427d5379f28e5d779ee
James De Angelis <jdeangelis83@gmail.com.com>
2015-02-22 09:48:25 -0800

    Finished project + readme

--- Mailbox/mailboxViewController.swift
+++ Mailbox/mailboxViewController.swift
@@ -9,40 +9,53 @@
 import UIKit
 
 class mailboxViewController: UIViewController {
+    
     @IBOutlet weak var mailScrollView: UIScrollView!
     @IBOutlet weak var messageView: UIImageView!
     @IBOutlet weak var messageContainer: UIView!
     @IBOutlet weak var laterIconView: UIImageView!
     @IBOutlet weak var archiveIconView: UIImageView!
+    @IBOutlet weak var messageFeed: UIImageView!
+    @IBOutlet weak var rescheduleView: UIImageView!
+    @IBOutlet weak var rescheduleButton: UIButton!
+    @IBOutlet weak var listView: UIImageView!
+    @IBOutlet weak var listButton: UIButton!
+    @IBOutlet weak var mailContainer: UIView!
 
     var messageStartX : CGFloat!
     var messagePanBegan : CGFloat!
     var laterIconStartX : CGPoint!
     var archiveIconStartX: CGFloat!
+    var messageBackgroundColor: UIColor!
     
     var finalMessagePosition : CGFloat!
     var finalaterIconPosition: CGFloat!
     var finalarchiveIconPosition: CGFloat!
     
-    var messageBackgroundColor: UIColor!
+    var edgePanBegan: CGFloat!
+    var finalMailContainerPosition: CGFloat!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
         mailScrollView.contentSize = CGSize(width: 320, height:1136)
         
         messageStartX = messageView.frame.origin.x
-//        laterIconStartX = laterIconView.frame.origin.x
         archiveIconStartX = archiveIconView.frame.origin.x
-        messageBackgroundColor = UIColor.grayColor()
+        rescheduleButton.hidden = true
+        listButton.hidden = true
         
-        laterIconStartX = CGPoint(x: messageView.frame.width - 30, y: messageView.center.y)
-         messageView.alpha = 0.5
+        laterIconView.center.x = 290
+        archiveIconView.center.x = 30
         
-//        messageBackgroundColor = messageContainer.backgroundColor
-        println("hello world!")
-}
+        // edge panning setup
+        var edgeGesture = UIScreenEdgePanGestureRecognizer(target: self, action: "onEdgePan:")
+        edgeGesture.edges = UIRectEdge.Left
+        mailContainer.addGestureRecognizer(edgeGesture)
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -53,71 +66,281 @@ class mailboxViewController: UIViewController {
         var location = sender.locationInView(view)
         var translation = sender.translationInView(view)
         var velocity = sender.velocityInView(view)
-        
-        println("velocity: \(velocity.x)")
-        println("translation x: \(translation.x)")
 
         if (sender.state == UIGestureRecognizerState.Began) {
             messagePanBegan = messageView.frame.origin.x
             
         } else if(sender.state == UIGestureRecognizerState.Changed) {
-            
             finalMessagePosition = messagePanBegan + translation.x
             
-//           SHORT SWIPE LEFT
-            if (messageView.center.x < 100 && messageView.center.x > -40) {
-                messageView.alpha = 0.5
-                messageContainer.backgroundColor = UIColor.blueColor()
-                laterIconView.center.x = messageView.center.x + 160 + 50
+            // begin swipe left
+            if (messageView.frame.origin.x < 0 && messageView.frame.origin.x > -60) {
+                laterIconView.alpha = -translation.x / 60
+            }
+            
+            // begin swipe right
+            if (messageView.frame.origin.x > 0 && messageView.frame.origin.x < 60) {
+                archiveIconView.alpha = translation.x / 60
+            }
+            
+            // Short swipe left
+            if (messageView.frame.origin.x < -60 && messageView.frame.origin.x > -180) {
+                messageContainer.backgroundColor = UIColor(red: (250/255.0), green: (211/255.0), blue: (51/255.0), alpha: 1.0)
+                laterIconView.image = UIImage(named: "later_icon")
+                laterIconView.center.x = messageView.center.x + 160 + 30
                 
-//          Long swipe left
-            } else if (velocity.x < 0 && translation.x < -150) {
-                messageContainer.backgroundColor = UIColor.cyanColor()
+            // long swipe left
+            } else if (messageView.frame.origin.x < -179) {
+                messageContainer.backgroundColor = UIColor(red: (216/255.0), green: (166/255.0), blue: (117/255.0), alpha: 1.0)
+                laterIconView.image = UIImage(named: "list_icon")
+                laterIconView.center.x = messageView.center.x + 160 + 30
                 
-            } else if (velocity.x > 0 && translation.x > 60) {
-                messageContainer.backgroundColor = UIColor.greenColor()
+            // Short swipe right
+            } else if (messageView.frame.origin.x > 60 && messageView.frame.origin.x < 180) {
+                messageContainer.backgroundColor = UIColor(red: (112/255.0), green: (217/255.0), blue: (98/255.0), alpha: 1.0)
+                
+                archiveIconView.image = UIImage(named: "archive_icon")
+                archiveIconView.center.x = messageView.center.x - 160 - 30
+            
+            // Long swipe right
+            } else if (messageView.frame.origin.x > 179) {
+                messageContainer.backgroundColor = UIColor(red: (235/255.0), green: (84/255.0), blue: (51/255.0), alpha: 1.0)
+                archiveIconView.image = UIImage(named: "delete_icon")
+                archiveIconView.center.x = messageView.center.x - 160 - 30
                 
             } else {
-                messageContainer.backgroundColor = UIColor.grayColor()
+                messageContainer.backgroundColor = UIColor(red: (227/255.0), green: (227/255.0), blue: (227/255.0), alpha: 1.0)
+                laterIconView.center.x = 290
+                archiveIconView.center.x = 30
             }
             
             messageView.frame.origin.x = finalMessagePosition
-//            laterIconView.frame.origin.x = finalaterIconPosition
-//            
+            
         } else if(sender.state == UIGestureRecognizerState.Ended) {
             
-            if (velocity.x < 0 && translation.x < -60) {
+            // complete pan for later action
+            if (messageView.frame.origin.x < -60 && messageView.frame.origin.x > -180) {
+                self.archiveIconView.alpha = 0
 
                 UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
-                    self.messageView.frame.origin.x = -300
-                }, completion: nil)
-                
-            } else if (velocity.x > 0 && translation.x > 60) {
-                UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
-                    self.messageView.frame.origin.x = 340
-                }, completion: nil)
+                    self.messageView.frame.origin.x = -320
+                    self.laterIconView.center.x = self.messageView.center.x + 15 + 20
+                    
+                    }, completion: { (Bool) -> Void in
+                        UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                            self.rescheduleView.alpha = 1
+                            self.rescheduleButton.hidden = false
+                            
+                        }, completion: nil)
+                    })
             
+            // complete scroll for list action
+            } else if (messageView.frame.origin.x < -180) {
+                UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                    self.messageView.frame.origin.x = -320
+                    self.laterIconView.center.x = self.messageView.center.x + 15 + 20
+                    
+                    }, completion: { (Bool) -> Void in
+                        UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                            self.listView.alpha = 1
+                            self.listButton.hidden = false
+                            
+                            }, completion: nil)
+                })
+            
+            // complete for done action
+             } else if (messageView.frame.origin.x > 60 && messageView.frame.origin.x < 180) {
+                UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                    
+                    self.messageView.frame.origin.x = 320
+                    self.archiveIconView.center.x = self.messageView.center.x + 15 + 20
+                    
+                    }, completion: { (Bool) -> Void in
+                        UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                            self.messageContainer.alpha = 0
+                            
+                            }, completion: { (Bool) -> Void in
+                                UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                                    self.messageFeed.frame.origin.y = self.messageFeed.frame.origin.y - self.messageView.frame.height
+                                    
+                                    }, completion: { (Bool) -> Void in
+                                        UIView.animateWithDuration(0.7, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                                            self.addMessage()
+                                            }, completion: nil)
+                                })
+                        })
+                })
+                
+            // complete for delete action
+            } else if (messageView.frame.origin.x > 180) {
+                UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                    self.messageView.frame.origin.x = 320
+                    self.archiveIconView.center.x = self.messageView.center.x + 15 + 20
+                    
+                    }, completion: { (Bool) -> Void in
+                        UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                            self.messageContainer.alpha = 0
+                            
+                            }, completion: { (Bool) -> Void in
+                                UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                                    self.messageFeed.frame.origin.y = self.messageFeed.frame.origin.y - self.messageView.frame.height
+                                    
+                                    }, completion: { (Bool) -> Void in
+                                        UIView.animateWithDuration(0.7, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                                            self.addMessage()
+                                            }, completion: nil)
+                                })
+                })
+            })
+                
+            // bounce back if not scrolled far enough
             } else {
-                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 15, options: nil, animations: { () -> Void in
-                    self.messageView.frame.origin.x = 20
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 15, options: nil, animations: { () -> Void in
+                    self.messageView.frame.origin.x = 0
+                    
                 }, completion: nil)
             }
             
         }
     }
     
+    
+    @IBAction func reschedulePressed(sender: AnyObject) {
+        UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+            self.rescheduleView.alpha = 0
+            self.rescheduleButton.hidden = true
+            
+            }, completion: { (Bool) -> Void in
+                UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                    self.messageContainer.alpha = 0
+                    
+                    }, completion: { (Bool) -> Void in
+                        self.removeMessage()
+                })
+        })
+        
+        UIView.animateWithDuration(1, delay: 1, options: .CurveLinear, animations: { () -> Void in
+            self.messageContainer.alpha = 1
+            self.messageView.frame.origin.x = 0
+            
+            }, completion: { (Bool) -> Void in
+                UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                    self.messageFeed.frame.origin.y = self.messageFeed.frame.origin.y + self.messageView.frame.height
+                    self.messageView.frame.origin.x = 0
+                    self.messageContainer.alpha = 1
+                    self.messageView.alpha = 1
+                    self.archiveIconView.alpha = 0
+                    self.laterIconView.alpha = 0
+                    }, completion: nil)
+            })
+
+        }
+    
+    @IBAction func listButtonPressed(sender: AnyObject) {
+        UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+            self.listView.alpha = 0
+            self.listButton.hidden = true
+            
+            }, completion: { (Bool) -> Void in
+                self.removeMessage()
+        })
+        
+        UIView.animateWithDuration(1, delay: 1, options: .CurveLinear, animations: { () -> Void in
+            self.messageContainer.alpha = 1
+            self.messageView.frame.origin.x = 0
+            
+            }, completion: { (Bool) -> Void in
+                UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                    self.messageFeed.frame.origin.y = self.messageFeed.frame.origin.y + self.messageView.frame.height
+                    self.messageView.frame.origin.x = 0
+                    self.messageContainer.alpha = 1
+                    self.messageView.alpha = 1
+                    self.archiveIconView.alpha = 0
+                    self.laterIconView.alpha = 0
+                    }, completion: nil)
+            })
+            
+        }
+    
+    // Animate the message away after successful swipe
+    func removeMessage() {
+        UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+            self.messageContainer.alpha = 0
+            
+            }, completion: { (Bool) -> Void in
+                UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                    self.messageFeed.frame.origin.y = self.messageFeed.frame.origin.y - self.messageView.frame.height
+                    
+                    }, completion: nil)
+        })
+    }
+    
+    // Return the message to it's beginning state
+    func addMessage() {
+        UIView.animateWithDuration(0, delay: 0, options: .CurveLinear, animations: { () -> Void in
+            self.messageContainer.alpha = 1
+            self.messageView.frame.origin.x = 0
+            self.messageView.alpha = 0
+            self.messageContainer.backgroundColor = UIColor(red: (227/255.0), green: (227/255.0), blue: (227/255.0), alpha: 1.0)
+            
+            }, completion: { (Bool) -> Void in
+                UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseInOut, animations: { () -> Void in
+                    self.messageFeed.frame.origin.y = self.messageFeed.frame.origin.y + self.messageView.frame.height
+                    self.messageContainer.alpha = 1
+                    self.messageView.alpha = 1
+                    }, completion: nil)
+        })
+    }
+    
     @IBAction func resetButtonPresserd(sender: AnyObject) {
-        messageView.center.x = view.frame.width / 2
+        
+        if (mailContainer.frame.origin.x == 0) {
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 15, options: nil, animations: { () -> Void in
+                self.mailContainer.frame.origin.x = self.mailContainer.frame.origin.x + 280
+            }, completion: nil)
+            } else if (mailContainer.frame.origin.x == 280) {
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 15, options: nil, animations: { () -> Void in
+                self.mailContainer.frame.origin.x = self.mailContainer.frame.origin.x - 280
+                }, completion: nil)
+            }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //Edge panning 
+    func onEdgePan(sender: UIScreenEdgePanGestureRecognizer) {
+        var translation = sender.translationInView(view)
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            edgePanBegan = mailContainer.frame.origin.x
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            finalMailContainerPosition = edgePanBegan + translation.x
+            mailContainer.frame.origin.x = finalMailContainerPosition
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            
+            // Spring back if not panned far enough
+            if (mailContainer.frame.origin.x < 100) {
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 15, options: nil, animations: { () -> Void in
+                    self.mailContainer.frame.origin.x = 0
+                    }, completion: nil)
+            
+            // Spring forward if panned far enough
+            } else if (mailContainer.frame.origin.x > 100) {
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 15, options: nil, animations: { () -> Void in
+                    self.mailContainer.frame.origin.x = 280
+                    }, completion: nil)
+            }
+        }
     }
-    */
+    
 
+
+/*
+// MARK: - Navigation
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+// Get the new view controller using segue.destinationViewController.
+// Pass the selected object to the new view controller.
+*/
+    
 }
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Codepath-week-3
+# Mailbox
+This is a sample of Mailbox swipe gestures and flow
+ 
+Time spent: 9 hours spent in total
+
+**Completed user stories:**
+
+_Required_
+
+On dragging the message left
+* [x] Required: Initial background set to grab
+* [x] Required: As the reschedule icon is revealed, starts semi-transparent and becomes fully opaque. If released at this point, the message returns to its initial position.
+* [x] Required: After 60 pts, the later icon starts moving with the translation and the background changes to yellow.
+* [x] Required: Upon release, the message continues to reveal the yellow background. When the animation it complete, it shows reschedule options.
+* [x] Required: After 260 pts, the icon changes to the list icon and the background color changes to brown
+* [x] Required: Upon release, the message continues to reveal the brown background. When the animation it complete, it shows list options.
+* [x] Required: User can tap to dismissing the reschedule or list options. After the reschedule or list options are dismissed, you see the message finish the hide animation.
+
+On dragging the message right
+* [x] Required: Initially, the revealed background color should be gray.
+* [x] Required: As the archive icon is revealed, it starts semi-transparent and becomes fully opaque. If released at this point, the message returns to its initial position.
+* [x] Required: After 60 pts, the archive icon should start moving with the translation and the background should change to green.
+* [x] Required: Upon release, the message should continue to reveal the green background. When the animation it complete, it should hide the message.
+* [x] Required: After 260 pts, the icon changes to the delete icon and the background color changes to red.
+* [x] Required: Upon release, the message continues to reveal the red background. When the animation it complete, it hides the message.
+
+_Optional_ 
+* [x] Optional: Panning from the edge reveals the menu
+* [x] Optional: If the menu is being revealed when the user lifts their finger, it continues revealing.
+* [x] Optional: If the menu is being hidden when the user lifts their finger, it continues hiding.
+
+**Walkthrough of all user stories:**
+
+![Video Walkthrough](walkthrough.gif)
